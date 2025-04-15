511e3bdec880ca645a8605676dae135a63e1192a
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-15 18:45:08 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -54,7 +54,7 @@ function page() {
            <h1 className='font-bold text-3xl text-cyan-500'>Welcome !!</h1>
            <p className='font-bold text-4xl text-cyan-600 max-sm:text-center'>I'm a Full Stack Developer</p>
            <div className="h-12 w-24 bg-cyan-200 mt-5 rounded-md">
-            <a href="https://drive.google.com/file/d/1WrJe0RrN2Rah6ZIP30y1jcCjseDdrvsp/view?usp=drive_link" target='_blank' className="font-bold h-full w-full flex items-center justify-center ">Resume</a>
+            <a href="https://drive.google.com/file/d/1NX1rq1WLdIhMTrKudHq7LfzIhIfC6dS6/view?usp=drive_link" target='_blank' className="font-bold h-full w-full flex items-center justify-center ">Resume</a>
            </div>
          </div>
          <div className="w-[50%] flex items-center justify-center max-sm:w-full">

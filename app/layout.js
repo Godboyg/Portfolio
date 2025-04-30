eb2d39f4fa9152931ffdac8571460d30d6bca462
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-30 07:05:44 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,9 @@ export default function RootLayout({ children }) {
         <title>
           Portfolio
         </title>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2915175777820694"
+          crossorigin="anonymous">
+        </script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
